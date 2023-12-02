a6b7822d704b2cafc7cb2faf32bf5e01a64efc73
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-02 08:06:14 +0100

    Day 2 - Star 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -16,31 +16,35 @@ fn get_input() -> Vec<Vec<u32>> {
 }
 
 fn get_input2() -> Vec<Vec<usize>> {
-	let input = fs::read_to_string(Path::new("./input/day1.input"))
+    let input = fs::read_to_string(Path::new("./input/day1.input"))
         .expect("Something went wrong with the input");
     let reg = Regex::new(r"(\d|one|two|three|four|five|six|seven|eight|nine)").unwrap();
-    let temp = input.trim().lines().map(|line| {
-    	let mut values = vec!();
-    	// The same match will be added multiple time, but since we only use the first and last match it shouldn't impact the final result
-    	for i in 0..line.len() {
-    		if let Some(cap) = reg.captures_at(line, i) {
-    			let num = match &cap[1] {
-    				"one" => 1,
-		    		"two" => 2,
-		    		"three" => 3,
-		    		"four" => 4,
-		    		"five" => 5,
-		    		"six" => 6,
-		    		"seven" => 7,
-		    		"eight" => 8,
-		    		"nine" => 9,
-		    		a => a.parse::<usize>().unwrap()
-		    	};
-		    	values.push(num);
-    		}
-    	}
-    	values
-    }).collect_vec();
+    let temp = input
+        .trim()
+        .lines()
+        .map(|line| {
+            let mut values = vec![];
+            // The same match will be added multiple time, but since we only use the first and last match it shouldn't impact the final result
+            for i in 0..line.len() {
+                if let Some(cap) = reg.captures_at(line, i) {
+                    let num = match &cap[1] {
+                        "one" => 1,
+                        "two" => 2,
+                        "three" => 3,
+                        "four" => 4,
+                        "five" => 5,
+                        "six" => 6,
+                        "seven" => 7,
+                        "eight" => 8,
+                        "nine" => 9,
+                        a => a.parse::<usize>().unwrap(),
+                    };
+                    values.push(num);
+                }
+            }
+            values
+        })
+        .collect_vec();
     temp
 }
 
--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,7 +1,55 @@
 use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+use itertools::Itertools;
+
+const MAX_CUBES: [usize; 3] = [12, 13, 14];
+
+fn get_input() -> Vec<Vec<[usize; 3]>> {
+    let input = fs::read_to_string(Path::new("./input/day2.input"))
+        .expect("Something went wrong when opening the input");
+    let mut all_games = vec![];
+    for line in input.trim().lines() {
+        let mut single_game = vec![];
+        for game in line.split(&[':', ';']).skip(1) {
+            let mut rgb = [0; 3];
+            for draw in game.split(',') {
+                let cubes = draw.split_whitespace().collect_vec();
+                let value = cubes[0].parse::<usize>().unwrap();
+                match cubes[1] {
+                    "red" => rgb[0] = value,
+                    "green" => rgb[1] = value,
+                    "blue" => rgb[2] = value,
+                    _ => unreachable!(),
+                }
+            }
+            single_game.push(rgb);
+        }
+        all_games.push(single_game);
+    }
+    all_games
+}
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 1 not ready");
+    let games = get_input();
+    let mut valid_games = 0;
+    for (id, game) in games.into_iter().enumerate() {
+        let max_cubes = game.iter().fold([0; 3], |mut acc, el| {
+            acc[0] = acc[0].max(el[0]);
+            acc[1] = acc[1].max(el[1]);
+            acc[2] = acc[2].max(el[2]);
+            acc
+        });
+        if max_cubes[0] <= MAX_CUBES[0]
+            && max_cubes[1] <= MAX_CUBES[1]
+            && max_cubes[2] <= MAX_CUBES[2]
+        {
+            valid_games += id + 1;
+        }
+    }
+    println!("The sum ids of valid games is {}", valid_games);
+    Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
