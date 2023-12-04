3977192ab152a99343f33da0586ce46b2c81c56c
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-04 08:04:19 +0100

    Day 4 - Star 1

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,7 +1,53 @@
 use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+use itertools::Itertools;
+
+fn get_input() -> Vec<(Vec<usize>, Vec<usize>)> {
+    let input = fs::read_to_string(Path::new("./input/day4.input"))
+        .expect("Something went wrong with the input");
+    input
+        .trim()
+        .lines()
+        .map(|line| {
+            let mut parts = line.split('|');
+            let winning = parts
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .skip(2)
+                .map(str::parse::<usize>)
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap();
+            let scratched = parts
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .map(str::parse::<usize>)
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap();
+            (winning, scratched)
+        })
+        .collect_vec()
+}
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 1 not ready");
+    let cards = get_input();
+    let mut result = 0;
+    for (winning, scratched) in cards {
+        let won = scratched
+            .iter()
+            .filter(|value| winning.contains(value))
+            .count();
+        if won > 0 {
+            result += 2_u32.pow(won as u32 - 1);
+        }
+    }
+
+    println!("All these cards are worth {:?} points", result);
+
+    Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
