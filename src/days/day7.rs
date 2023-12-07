9f63ac84e0d66c9bd53c9c9992456883b681fa4a
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-07 14:24:27 +0100

    Day 7 - Star 2

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -33,26 +33,32 @@ fn get_input() -> Vec<(Vec<char>, usize)> {
         .collect_vec()
 }
 
-fn get_card_score(card: &char) -> usize {
+fn get_card_score(card: &char, is_joker: bool) -> usize {
     match card {
-        '2' => 0,
-        '3' => 1,
-        '4' => 2,
-        '5' => 3,
-        '6' => 4,
-        '7' => 5,
-        '8' => 6,
-        '9' => 7,
-        'T' => 8,
-        'J' => 9,
-        'Q' => 10,
-        'K' => 11,
-        'A' => 12,
+        '2' => 1,
+        '3' => 2,
+        '4' => 3,
+        '5' => 4,
+        '6' => 5,
+        '7' => 6,
+        '8' => 7,
+        '9' => 8,
+        'T' => 9,
+        'J' => {
+            if is_joker {
+                0
+            } else {
+                10
+            }
+        }
+        'Q' => 11,
+        'K' => 12,
+        'A' => 13,
         _ => unreachable!(),
     }
 }
 
-fn get_hand_name(hand: &[char]) -> Hands {
+fn get_hand_name(hand: &[char], is_joker: bool) -> Hands {
     let mut card_count: HashMap<&char, usize> = HashMap::new();
     for card in hand {
         card_count
@@ -60,6 +66,22 @@ fn get_hand_name(hand: &[char]) -> Hands {
             .and_modify(|count| *count += 1)
             .or_insert(1);
     }
+
+    if is_joker {
+        if let Some(joker_count) = card_count.remove(&'J') {
+            if card_count.is_empty() {
+                return Hands::FiveOAK;
+            }
+            let (max_card, _) = card_count
+                .iter()
+                .max_by(|(_, val_a), (_, val_b)| val_a.cmp(val_b))
+                .unwrap();
+            card_count
+                .entry(*max_card)
+                .and_modify(|elem| *elem += joker_count);
+        }
+    }
+
     let mut card_count = card_count.values().cloned().collect_vec();
     card_count.sort();
     match card_count.len() {
@@ -86,15 +108,16 @@ fn get_hand_name(hand: &[char]) -> Hands {
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let mut input = get_input();
     input.sort_by(|(left_hand, _), (right_hand, _)| {
-        let left_hand_name = get_hand_name(left_hand);
-        let right_hand_name = get_hand_name(right_hand);
+        let left_hand_name = get_hand_name(left_hand, false);
+        let right_hand_name = get_hand_name(right_hand, false);
 
         if left_hand_name != right_hand_name {
             left_hand_name.cmp(&right_hand_name)
         } else {
             for i in 0..5 {
                 if left_hand[i] != right_hand[i] {
-                    return get_card_score(&left_hand[i]).cmp(&get_card_score(&right_hand[i]));
+                    return get_card_score(&left_hand[i], false)
+                        .cmp(&get_card_score(&right_hand[i], false));
                 }
             }
             Ordering::Equal
@@ -109,5 +132,27 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 2 not ready");
+    let mut input = get_input();
+    input.sort_by(|(left_hand, _), (right_hand, _)| {
+        let left_hand_name = get_hand_name(left_hand, true);
+        let right_hand_name = get_hand_name(right_hand, true);
+
+        if left_hand_name != right_hand_name {
+            left_hand_name.cmp(&right_hand_name)
+        } else {
+            for i in 0..5 {
+                if left_hand[i] != right_hand[i] {
+                    return get_card_score(&left_hand[i], true)
+                        .cmp(&get_card_score(&right_hand[i], true));
+                }
+            }
+            Ordering::Equal
+        }
+    });
+    let mut score = 0;
+    for (rank, (_, bet)) in input.into_iter().enumerate() {
+        score += (rank + 1) * bet;
+    }
+    println!("Total winnings (jokers included) are: {:?}", score);
+    Ok(())
 }
