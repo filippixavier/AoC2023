6c24b429614b6726a45398720736ed8ad4a3e7e5
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-09 07:33:33 +0100

    Day 9 - Star 1

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -1,7 +1,50 @@
 use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+use itertools::Itertools;
+
+fn get_input() -> Vec<Vec<isize>> {
+    let input = fs::read_to_string(Path::new("./input/day9.input"))
+        .expect("Something went wrong with the input");
+    input
+        .trim()
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(str::parse::<isize>)
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap()
+        })
+        .collect_vec()
+}
+
+fn predict_next(input: &[isize]) -> isize {
+    if input.iter().all(|&value| value == 0) {
+        0
+    } else {
+        let mut sub_input = vec![];
+        for i in 1..input.len() {
+            sub_input.push(input[i] - input[i - 1]);
+        }
+        input.last().unwrap() + predict_next(&sub_input)
+    }
+}
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 1 not ready");
+    let readings = get_input();
+
+    let predicted = readings
+        .iter()
+        .map(|input| predict_next(input))
+        .collect_vec();
+
+    println!(
+        "The sum of all predicted inputs in the history is: {}",
+        predicted.iter().sum::<isize>()
+    );
+
+    Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
