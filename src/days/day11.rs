3b35851580b568f323bc0a69a7010b897bb9e8cb
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-11 09:52:38 +0100

    Day 11 - Star 2

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -15,10 +15,10 @@ fn get_input() -> Vec<Vec<char>> {
         .collect_vec()
 }
 
-fn expand_universe(universe: Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn get_empties(universe: &Vec<Vec<char>>) -> (Vec<usize>, Vec<usize>) {
     let mut empty_cols = vec![];
     'outer: for i in 0..universe[0].len() {
-        for line in &universe {
+        for line in universe {
             if line[i] == '#' {
                 continue 'outer;
             }
@@ -37,6 +37,11 @@ fn expand_universe(universe: Vec<Vec<char>>) -> Vec<Vec<char>> {
             }
         })
         .collect_vec();
+    (empty_lines, empty_cols)
+}
+
+fn expand_universe(universe: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let (empty_lines, empty_cols) = get_empties(&universe);
 
     let expanded_cols = universe[0].len() + empty_cols.len();
 
@@ -104,5 +109,60 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 2 not ready");
+    let universe = get_input();
+    let (empty_lines, empty_cols) = get_empties(&universe);
+    let galaxies = universe
+        .iter()
+        .enumerate()
+        .filter_map(|(line_no, line)| {
+            let coordinates = line
+                .iter()
+                .enumerate()
+                .filter_map(|(col_no, value)| {
+                    if *value == '.' {
+                        None
+                    } else {
+                        Some((line_no, col_no))
+                    }
+                })
+                .collect_vec();
+            if coordinates.is_empty() {
+                None
+            } else {
+                Some(coordinates)
+            }
+        })
+        .flatten()
+        .collect_vec();
+
+    let shortests = galaxies
+        .iter()
+        .combinations(2)
+        .map(|combo| {
+            let (start, end) = (combo[0], combo[1]);
+            let (start_line, start_col, end_line, end_col) = (
+                start.0.min(end.0),
+                start.1.min(end.1),
+                start.0.max(end.0),
+                start.1.max(end.1),
+            );
+            let empty_lines_count = empty_lines
+                .iter()
+                .filter(|&&line_no| line_no > start_line && line_no < end_line)
+                .count();
+            let empty_cols_count = empty_cols
+                .iter()
+                .filter(|&&col_no| col_no > start_col && col_no < end_col)
+                .count();
+            let standard_dist = end_line - start_line + end_col - start_col;
+            standard_dist + (empty_cols_count + empty_lines_count) * 999_999 // We've already counted the empty lines/cols once
+        })
+        .collect_vec();
+
+    println!(
+        "The sums of the shortest paths is {}",
+        shortests.iter().sum::<usize>()
+    );
+
+    Ok(())
 }
