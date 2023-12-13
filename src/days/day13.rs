8f4264a89048cdf96d6e9b223a63e47ea387f0b3
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-13 12:11:02 +0100

    Day 13 - Star 2

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -79,5 +79,68 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 2 not ready");
+    let fields = get_input();
+    let mut horizontal_mirrors = 0usize;
+    let mut vertical_mirrors = 0usize;
+
+    'field: for field in fields {
+        let width = field[0].len();
+        let height = field.len();
+        // horizontal flip detection with exactly 1 error
+        for i in 1..height {
+            let up = field.iter().take(i).rev();
+            let down = field.iter().skip(i);
+            let mut error_counter = 0;
+            for (line_up, line_down) in up.zip(down) {
+                error_counter +=
+                    line_up
+                        .iter()
+                        .zip(line_down.iter())
+                        .fold(
+                            0,
+                            |acc, (up_tile, down_tile)| {
+                                if up_tile != down_tile {
+                                    acc + 1
+                                } else {
+                                    acc
+                                }
+                            },
+                        );
+                if error_counter > 1 {
+                    break;
+                }
+            }
+            if error_counter == 1 {
+                horizontal_mirrors += i;
+                continue 'field;
+            }
+        }
+        // vertical flip detection
+        for j in 1..width {
+            let mut error_counter = 0;
+            for line in field.iter() {
+                let left = line.iter().take(j).rev();
+                let right = line.iter().skip(j);
+                error_counter += left.zip(right).fold(0, |acc, (left_tile, right_tile)| {
+                    if left_tile != right_tile {
+                        acc + 1
+                    } else {
+                        acc
+                    }
+                });
+                if error_counter > 1 {
+                    break;
+                }
+            }
+            if error_counter == 1 {
+                vertical_mirrors += j;
+                continue 'field;
+            }
+        }
+    }
+    println!(
+        "After fixing the smudge, the value obtained after summarizing all notes is: {}",
+        vertical_mirrors + 100 * horizontal_mirrors
+    );
+    Ok(())
 }
