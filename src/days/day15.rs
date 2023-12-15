e4b5a91092bf97e88379888416925c84ba32a536
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-15 10:13:43 +0100

    Day 15 - Star 1

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,7 +1,29 @@
 use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+fn get_input() -> Vec<String> {
+    let input = fs::read_to_string(Path::new("./input/day15.input"))
+        .expect("Something went wrong with the input");
+    return input
+        .trim()
+        .split(',')
+        .map(|elem| elem.to_string())
+        .collect();
+}
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 1 not ready");
+    let input_sequence = get_input();
+    println!(
+        "Total hash value of the input: {}",
+        input_sequence.iter().fold(0, |total, step| {
+            total
+                + step
+                    .chars()
+                    .fold(0, |acc, elem| ((acc + (elem as usize)) * 17) % 256)
+        })
+    );
+    Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
