34e5fec3b7660c5723458f948bf9fc87667d246c
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-23 22:27:39 +0100

    Day 23 - Star 2

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -21,7 +21,7 @@ struct Coordinate {
     y: usize,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Facing {
     Up,
     Left,
@@ -62,8 +62,6 @@ fn get_input() -> Vec<Vec<Tile>> {
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let hike_map = get_input();
-
-    // let mut intersections_memoizers: HashMap<Coordinate, (usize, usize)> = HashMap::new();
 
     let start_row = hike_map[0]
         .iter()
@@ -186,5 +184,95 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 2 not ready");
+    let hike_map: Vec<Vec<Tile>> = get_input()
+        .into_iter()
+        .map(|line| {
+            line.into_iter()
+                .map(|tile| if tile == Rock { Rock } else { Flat })
+                .collect()
+        })
+        .collect();
+
+    let start_row = hike_map[0]
+        .iter()
+        .position(|tile| *tile == Flat)
+        .unwrap_or(0);
+    let mut hikers: Vec<(Hiker, HashSet<Coordinate>)> = vec![(
+        Hiker {
+            facing: Down,
+            position: Coordinate { x: 0, y: start_row },
+            step_count: 0,
+        },
+        HashSet::new(),
+    )];
+
+    let mut max_step = 0;
+
+    while let Some((mut hiker, mut visited_coordinates)) = hikers.pop() {
+        loop {
+            let next_coordinate = match hiker.facing {
+                Up => Coordinate {
+                    x: hiker.position.x - 1,
+                    y: hiker.position.y,
+                },
+                Left => Coordinate {
+                    x: hiker.position.x,
+                    y: hiker.position.y - 1,
+                },
+                Down => Coordinate {
+                    x: hiker.position.x + 1,
+                    y: hiker.position.y,
+                },
+                Right => Coordinate {
+                    x: hiker.position.x,
+                    y: hiker.position.y + 1,
+                },
+            };
+            let tile = hike_map[next_coordinate.x][next_coordinate.y];
+            if tile == Rock {
+                break;
+            }
+
+            if !visited_coordinates.insert(next_coordinate) {
+                break;
+            }
+            hiker.position = next_coordinate;
+            hiker.step_count += 1;
+
+            if hiker.position.x == hike_map.len() - 1 {
+                max_step = max_step.max(hiker.step_count);
+                break;
+            }
+
+            let next_facing: Vec<Facing> = vec![Up, Down, Left, Right]
+                .into_iter()
+                .filter(|&face| match face {
+                    Up => {
+                        hiker.facing != Down
+                            && hike_map[hiker.position.x - 1][hiker.position.y] != Rock
+                    }
+                    Down => {
+                        hiker.facing != Up
+                            && hike_map[hiker.position.x + 1][hiker.position.y] != Rock
+                    }
+                    Left => {
+                        hiker.facing != Right
+                            && hike_map[hiker.position.x][hiker.position.y - 1] != Rock
+                    }
+                    Right => {
+                        hiker.facing != Left
+                            && hike_map[hiker.position.x][hiker.position.y + 1] != Rock
+                    }
+                })
+                .collect();
+            hiker.facing = next_facing[0];
+            for other_facing in next_facing.into_iter().skip(1) {
+                let mut other_hiker = hiker;
+                other_hiker.facing = other_facing;
+                hikers.push((other_hiker, visited_coordinates.clone()));
+            }
+        }
+    }
+    println!("Since the trails are surprisingly dry, 🏔️ The longest, most scenic view is, in fact, {} steps long", max_step);
+    Ok(())
 }
