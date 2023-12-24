36d20c140deb92be151263faa8a0b9088295639b
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-24 03:30:05 +0100

    Day 22 - Star 2

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::error::Error;
 use std::fs;
 use std::path::Path;
@@ -50,29 +50,31 @@ fn get_input() -> Vec<Brick> {
         .collect()
 }
 
-pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    let mut bricks = get_input();
+type Tower = HashMap<usize, (HashSet<usize>, HashSet<usize>)>;
+
+fn get_tower(mut bricks: Vec<Brick>) -> Tower {
     bricks.sort_by(|brick_a, brick_b| brick_a.start.z.cmp(&brick_b.start.z));
+
     let mut highest = 0;
     let mut bump_map: HashMap<usize, HashMap<(usize, usize), usize>> = HashMap::new();
 
-    let mut support_system: HashMap<usize, (HashSet<usize>, HashSet<usize>)> = HashMap::new();
+    let mut tower: HashMap<usize, (HashSet<usize>, HashSet<usize>)> = HashMap::new();
 
     for (index, brick) in bricks.iter().enumerate() {
         let height = brick.end.z - brick.start.z;
         let mut start_z = 0;
 
-        support_system.insert(index, (HashSet::new(), HashSet::new()));
+        tower.insert(index, (HashSet::new(), HashSet::new()));
 
         for z_check in (0..=highest).rev() {
             for x in brick.start.x..=brick.end.x {
                 for y in brick.start.y..=brick.end.y {
                     if let Some(brick_id) = bump_map.entry(z_check).or_default().get(&(x, y)) {
                         start_z = z_check + 1;
-                        support_system.entry(index).and_modify(|(supported_by, _)| {
+                        tower.entry(index).and_modify(|(supported_by, _)| {
                             supported_by.insert(*brick_id);
                         });
-                        support_system.entry(*brick_id).and_modify(|(_, support)| {
+                        tower.entry(*brick_id).and_modify(|(_, support)| {
                             support.insert(index);
                         });
                     }
@@ -94,8 +96,11 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
             }
         }
     }
+    tower
+}
 
-    let unremovable: HashSet<_> = support_system.values().fold(
+fn get_unremovable_bricks(tower: &Tower) -> HashSet<usize> {
+    tower.values().fold(
         HashSet::<usize>::new(),
         |mut acc: HashSet<_>, (supported_by, _)| {
             if supported_by.len() == 1 {
@@ -103,15 +108,44 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
             }
             acc
         },
-    );
+    )
+}
+
+pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
+    let tower = get_tower(get_input());
+
+    let unremovable = get_unremovable_bricks(&tower);
 
     println!(
         "🧱💥 We can safely vaporize {} bricks",
-        support_system.len() - unremovable.len()
+        tower.len() - unremovable.len()
     );
     Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    unimplemented!("Star 2 not ready");
+    let tower = get_tower(get_input());
+    let unremovable: Vec<usize> = get_unremovable_bricks(&tower).iter().cloned().collect();
+
+    let mut drop_count = 0;
+
+    for element in unremovable.iter().rev() {
+        let mut removed = HashSet::from([*element]);
+        let mut to_check: VecDeque<usize> = tower.get(element).unwrap().1.iter().cloned().collect();
+
+        while let Some(child_id) = to_check.pop_front() {
+            let (supported_by, support) = tower.get(&child_id).unwrap();
+            if supported_by.is_subset(&removed) {
+                removed.insert(child_id);
+                to_check.extend(support.iter());
+            }
+        }
+        drop_count += removed.len() - 1;
+    }
+
+    println!(
+        "🧱💥 By vaporizing the support bricks, we can bring down up to {} bricks",
+        drop_count
+    );
+    Ok(())
 }
