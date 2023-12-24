72fbaeff8eec8bd568e4ee03d2752b467e74cc87
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2023-12-24 02:44:11 +0100

    Day 23 - Star 2 Optimization
    Managed to bring down the execution time from 2h to 18~20minutes (both on --release)

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs;
 use std::path::Path;
@@ -63,14 +63,14 @@ fn get_input() -> Vec<Vec<Tile>> {
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let hike_map = get_input();
 
-    let start_row = hike_map[0]
+    let start_col = hike_map[0]
         .iter()
         .position(|tile| *tile == Flat)
         .unwrap_or(0);
     let mut hikers: Vec<(Hiker, HashSet<Coordinate>)> = vec![(
         Hiker {
             facing: Down,
-            position: Coordinate { x: 0, y: start_row },
+            position: Coordinate { x: 0, y: start_col },
             step_count: 0,
         },
         HashSet::new(),
@@ -193,86 +193,116 @@ pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
         })
         .collect();
 
-    let start_row = hike_map[0]
+    let start_col = hike_map[0]
         .iter()
         .position(|tile| *tile == Flat)
         .unwrap_or(0);
-    let mut hikers: Vec<(Hiker, HashSet<Coordinate>)> = vec![(
+
+    let mut hikers = vec![(
         Hiker {
-            facing: Down,
-            position: Coordinate { x: 0, y: start_row },
+            position: Coordinate { x: 0, y: start_col },
             step_count: 0,
+            facing: Down,
         },
-        HashSet::new(),
+        Coordinate { x: 0, y: start_col },
     )];
 
-    let mut max_step = 0;
+    let mut network: HashMap<Coordinate, HashMap<Coordinate, usize>> = HashMap::new();
 
-    while let Some((mut hiker, mut visited_coordinates)) = hikers.pop() {
+    let mut visited_crossroad = HashSet::new();
+
+    let turns = [Up, Down, Left, Right];
+    while let Some((mut hiker, mut last_node)) = hikers.pop() {
         loop {
-            let next_coordinate = match hiker.facing {
-                Up => Coordinate {
-                    x: hiker.position.x - 1,
-                    y: hiker.position.y,
-                },
-                Left => Coordinate {
-                    x: hiker.position.x,
-                    y: hiker.position.y - 1,
-                },
-                Down => Coordinate {
-                    x: hiker.position.x + 1,
-                    y: hiker.position.y,
-                },
-                Right => Coordinate {
-                    x: hiker.position.x,
-                    y: hiker.position.y + 1,
-                },
+            let mut next_pos = hiker.position;
+            match hiker.facing {
+                Up => {
+                    next_pos.x -= 1;
+                }
+                Down => {
+                    next_pos.x += 1;
+                }
+                Left => {
+                    next_pos.y -= 1;
+                }
+                Right => {
+                    next_pos.y += 1;
+                }
             };
-            let tile = hike_map[next_coordinate.x][next_coordinate.y];
-            if tile == Rock {
-                break;
-            }
+            hiker.position = next_pos;
 
-            if !visited_coordinates.insert(next_coordinate) {
-                break;
-            }
-            hiker.position = next_coordinate;
             hiker.step_count += 1;
 
             if hiker.position.x == hike_map.len() - 1 {
-                max_step = max_step.max(hiker.step_count);
+                let node = network.entry(last_node).or_default();
+                node.insert(hiker.position, hiker.step_count);
                 break;
             }
 
-            let next_facing: Vec<Facing> = vec![Up, Down, Left, Right]
-                .into_iter()
-                .filter(|&face| match face {
-                    Up => {
-                        hiker.facing != Down
-                            && hike_map[hiker.position.x - 1][hiker.position.y] != Rock
-                    }
-                    Down => {
-                        hiker.facing != Up
-                            && hike_map[hiker.position.x + 1][hiker.position.y] != Rock
-                    }
-                    Left => {
-                        hiker.facing != Right
-                            && hike_map[hiker.position.x][hiker.position.y - 1] != Rock
-                    }
-                    Right => {
-                        hiker.facing != Left
-                            && hike_map[hiker.position.x][hiker.position.y + 1] != Rock
+            let neighbors: Vec<&Facing> = turns
+                .iter()
+                .filter(|&&direction| {
+                    if hiker.facing == Up && direction == Down
+                        || hiker.facing == Down && direction == Up
+                        || hiker.facing == Left && direction == Right
+                        || hiker.facing == Right && direction == Left
+                    {
+                        false
+                    } else {
+                        let tile = match direction {
+                            Up => hike_map[hiker.position.x - 1][hiker.position.y],
+                            Down => hike_map[hiker.position.x + 1][hiker.position.y],
+                            Left => hike_map[hiker.position.x][hiker.position.y - 1],
+                            Right => hike_map[hiker.position.x][hiker.position.y + 1],
+                        };
+                        tile != Rock
                     }
                 })
                 .collect();
-            hiker.facing = next_facing[0];
-            for other_facing in next_facing.into_iter().skip(1) {
-                let mut other_hiker = hiker;
-                other_hiker.facing = other_facing;
-                hikers.push((other_hiker, visited_coordinates.clone()));
+
+            if neighbors.len() > 1 {
+                let node = network.entry(last_node).or_default();
+                node.insert(hiker.position, hiker.step_count);
+                let node = network.entry(hiker.position).or_default();
+                node.insert(last_node, hiker.step_count);
+                hiker.step_count = 0;
+                last_node = hiker.position;
+
+                if !visited_crossroad.insert(hiker.position) {
+                    break;
+                }
+            }
+
+            hiker.facing = *neighbors[0];
+
+            for dir in neighbors.into_iter().skip(1) {
+                let mut new_hiker = hiker;
+                new_hiker.facing = *dir;
+
+                hikers.push((new_hiker, last_node));
             }
         }
     }
+
+    let mut to_test: Vec<(Coordinate, HashSet<Coordinate>, usize)> =
+        vec![(Coordinate { x: 0, y: start_col }, HashSet::new(), 0)];
+    let mut max_step = 0;
+
+    while let Some((current, mut visited, total)) = to_test.pop() {
+        if !visited.insert(current) {
+            continue;
+        }
+        if let Some(neighbors) = network.get(&current) {
+            for (coordinate, steps) in neighbors.iter() {
+                if network.contains_key(coordinate) {
+                    to_test.push((*coordinate, visited.clone(), steps + total));
+                } else {
+                    max_step = max_step.max(total + *steps);
+                }
+            }
+        }
+    }
+
     println!("Since the trails are surprisingly dry, 🏔️ The longest, most scenic view is, in fact, {} steps long", max_step);
     Ok(())
 }
